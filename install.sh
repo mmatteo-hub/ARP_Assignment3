b2d2b6168ea676e2f4b0e266197aaad9094f6931
mose247 <91455159+mose247@users.noreply.github.com>
2022-02-14 12:19:41 +0100

    Updated install.sh to compile drone_RM1.c

--- install.sh
+++ install.sh
@@ -25,7 +25,8 @@ gcc -c $SOURCES_DIR/logger/logger.c       -o $LOGGER_LIB
 # Compiling sources with requires libraries
 gcc $SOURCES_DIR/master/master.c       -o $EXES_DIR/master      $DRONE_API $LOGGER_LIB -std=gnu99 
 gcc $SOURCES_DIR/drone_ms3/drone_ms3.c -o $EXES_DIR/drone_ms3   $DRONE_API $LOGGER_LIB
+gcc $SOURCES_DIR/drone_RM1/drone_RM1.c -o $EXES_DIR/drone_RM1 $DRONE_API
 #gcc $SOURCES_DIR/drone_DF21/drone_DF21.c -o $EXES_DIR/drone_DF21 $DRONE_API
 #gcc $SOURCES_DIR/drone_MS_8/drone_MS_8.c -o $EXES_DIR/drone_MS_8 $DRONE_API
-#gcc $SOURCES_DIR/drone_RM_1/drone_RM_1.c -o $EXES_DIR/drone_RM_1 $DRONE_API
+
 
