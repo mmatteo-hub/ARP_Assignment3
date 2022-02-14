762b314b98a5c62a6d8a2eea00dfe9596e2e3cb1
Mattia Musumeci <mattia.musumeci@yahoo.it>
2022-02-14 14:30:50 +0100

    added other comments

--- project/help.sh
+++ project/help.sh
@@ -1,3 +1,6 @@
+# -d : helps the user navigate (prints help on bottom)
+# -p : clears the console before showing the text
+# -c : displays the text by overwriting the previous one
 cat \
 project_description.txt <(echo) \
 sources/drone_api/drone_api.txt <(echo) \
