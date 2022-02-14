bc1b8f6dd0417508dcca437c2797f16255b531e8
Mattia Musumeci <mattia.musumeci@yahoo.it>
2022-02-14 23:22:37 +0100

    cose

--- project/run.sh
+++ project/run.sh
@@ -8,5 +8,5 @@ gnome-terminal -- "$EXES_DIR/drone_ms3"
 gnome-terminal -- "$EXES_DIR/drone_RM1" "37" "20" "6" "$LOGFILES_DIR/log_drone_RM1.txt"
 gnome-terminal -- "$EXES_DIR/drone_MS8" "40" "20" "15"
 gnome-terminal -- "$EXES_DIR/drone_DM3" "42" "18" "$LOGFILES_DIR/log_drone_DM3.txt" 
-gnome-terminal -- "$EXES_DIR/drone_DF21" "1" "1" "$LOGFILES_DIR/logfile_drone_DF21.txt"
+gnome-terminal -- "$EXES_DIR/drone_DF21" "1" "1" "$LOGFILES_DIR/log_drone_DF21.txt"
 
