3ee3cfe2039943397fe3c0d55c48cfc58a702d01
mose247 <91455159+mose247@users.noreply.github.com>
2022-02-14 12:17:09 +0100

    Updated run.sh to run drone_RM1

--- project/run.sh
+++ project/run.sh
@@ -5,3 +5,4 @@ LOGFILES_DIR="./sources/logfile"
 # Terminals are launched for each process
 gnome-terminal -- "$EXES_DIR/master" "$LOGFILES_DIR/log_master.txt"
 gnome-terminal -- "$EXES_DIR/drone_ms3"
+gnome-terminal -- "$EXES_DIR/drone_RM1" "10" "10" "6" "$LOGFILES_DIR/log_drone_RM1.txt"
