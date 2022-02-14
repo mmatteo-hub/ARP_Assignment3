1749c24b4f36e7854b648fdda2eb174e7bb27b07
Mo-AH <91679281+Mo-AH@users.noreply.github.com>
2022-02-14 13:49:23 +0100

    Update run.sh
    Added drone_MS8

--- project/run.sh
+++ project/run.sh
@@ -6,3 +6,5 @@ LOGFILES_DIR="./sources/logfile"
 gnome-terminal -- "$EXES_DIR/master" "$LOGFILES_DIR/log_master.txt"
 gnome-terminal -- "$EXES_DIR/drone_ms3"
 gnome-terminal -- "$EXES_DIR/drone_RM1" "10" "10" "6" "$LOGFILES_DIR/log_drone_RM1.txt"
+gnome-terminal -- "$EXES_DIR/drone_MS8" "40" "20" "12" 
+
