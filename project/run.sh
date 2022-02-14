0e9af7f012ee9f8f962f97f92a863c6fd94e4a4f
Mo-AH <91679281+Mo-AH@users.noreply.github.com>
2022-02-14 21:56:13 +0100

    restored drone_MS8 line

--- project/run.sh
+++ project/run.sh
@@ -6,6 +6,6 @@ LOGFILES_DIR="./sources/logfile"
 gnome-terminal -- "$EXES_DIR/master" "$LOGFILES_DIR/log_master.txt"
 gnome-terminal -- "$EXES_DIR/drone_ms3"
 gnome-terminal -- "$EXES_DIR/drone_RM1" "37" "20" "6" "$LOGFILES_DIR/log_drone_RM1.txt"
-#gnome-terminal -- "$EXES_DIR/drone_MS8" "40" "20" "12"
+gnome-terminal -- "$EXES_DIR/drone_MS8" "40" "20" "15"
 gnome-terminal -- "$EXES_DIR/drone_DM3" "42" "18" "$LOGFILES_DIR/log_drone_DM3.txt" 
 
